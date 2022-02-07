0af3ba0d76003fb965a206a94a1c6eb98a8758a6
Mohammed Al-Kaf <m.alkaff11@gmail.com>
2022-02-06 23:08:25 -0500

    Fix typo in Haya Project

--- src/portfolio.js
+++ src/portfolio.js
@@ -48,7 +48,7 @@ const header = {
         'https://i.ibb.co/cFdRQv9/iphone-7-minimal-mockup-free-preview-1.png'
       ],
       description:
-        `(Work on Progress) Haya is an MERN stack e-commerece website for shopping with a responsive design for all devices. The user can log in/sign up, check the products available at the store, 
+        `(Work on Progress) Haya is a MERN stack e-commerece website for shopping with a responsive design for all devices. The user can log in/sign up, check the products available at the store, 
         add procusts to cart and check out and process pyament via Stripe.
         `,
       stack: ['ReactJS', 'JavaScript', 'NodeJS', 'MongoDB', 'ExpressJS', "JWT","Stripe","Redux","Styled-components"],
